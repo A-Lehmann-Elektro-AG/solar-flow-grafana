6d7934101336fc7ed111de0cd4c433023726665c
d1msk1y <exect81@gmail.com>
2024-03-15 16:17:56 +0100

    Prepare for publish, build

--- src/components/energyFlow/energyPoint.tsx
+++ src/components/energyFlow/energyPoint.tsx
@@ -60,7 +60,7 @@ function legendComponent(fontColor: string, label: string) {
         <circle className="z-5" cx="100" cy="100" r={baseRadius} style={props.style} strokeWidth="1.5"
                 fill={...fill}/>
         <text fontSize={18} fill={fontColor} x="100" y="15"
-              textAnchor="middle">{props.value + "kW"}</text>
+              textAnchor="middle">{props.value + " kW"}</text>
         {legend}
         <svg xmlns="http://www.w3.org/2000/svg" x='60' y="60" height="80" fill={iconColor} viewBox="0 -960 960 960"
              width="80">
